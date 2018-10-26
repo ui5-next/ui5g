2e6ddcbadc48c3a70a29f234093aa19f2683cfe9
Theo Sun <theo.sun@sap.com>
2018-10-26 21:45:11 +0800

    feat(webpack): almost full support

---
+++ generators/app/templates/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: "production",
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        include: [
+          path.resolve(__dirname, "./src")
+        ],
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: "ui5-next-loader?sourceRoot=./src&namespace=<% namespace %>"
+          },
+          {
+            loader: 'babel-loader?sourceRoot=./src'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      "./src/index.html",
+      "./src/manifest.json",
+      { from: "./src/i18n", to: "i18n" }
+    ])
+  ]
+};
