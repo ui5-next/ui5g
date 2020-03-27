fb36c7d797bcf9af11c00bd748e6b546136be036
Theo Sun <theo.sun@sap.com>
2020-03-27 11:25:32 +0800

    chore: jest test add ui5 version test

--- generators/app/templates/jest-test/test/index.test.js
+++ generators/app/templates/jest-test/test/index.test.js
@@ -2,17 +2,21 @@ const packageJson = require("../package");
 
 describe("app general tests", () => {
 
-  beforeAll(async() => {
+  beforeAll(async () => {
     page.setDefaultTimeout(3600 * 1000);
   });
 
-  beforeEach(async() => {
+  beforeEach(async () => {
     jest.setTimeout(100 * 1000);
     await page.goto("http://localhost:3000");
   });
 
-  it("should have a title", async() => {
+  it("should have a title", async () => {
     await expect(await page.title()).toEqual(packageJson.displayName);
+  });
+
+  it("should have a version", async () => {
+    await expect(await page.evaluate(() => sap.ui.version)).not.toBeUndefined()
   });
 
 });
